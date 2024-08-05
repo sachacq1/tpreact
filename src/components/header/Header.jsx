69499a4f4aaa6913e42d17cd6e3731d9c86f84f8
sachacq1 <sachacq1@gmail.com>
2024-08-05 16:42:53 -0300

    change css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,21 +5,21 @@ import "./Header.css";
 
 const Header = () => {
 
-    return (
-        <header className="header">
+  return (
+    <header className="header">
       <div className="logo">
-      <i className='bx bxs-mouse-alt'></i>
+        <i class='bx bx-code-alt'></i>
       </div>
       <nav className="nav-menu">
         <ul>
           <li><Link to="/">Inicio</Link></li>
-          <li><Link  to="/contact">Contacto</Link></li>
-          <li><Link  to="/portfolio">Portfolio</Link></li>
+          <li><Link to="/contact">Contacto</Link></li>
+          <li><Link to="/portfolio">Portfolio</Link></li>
         </ul>
       </nav>
     </header>
-    )
-    
+  )
+
 };
 
 export default Header;
--- src/views/portfolio/Portfolio..jsx
+++ src/views/portfolio/Portfolio..jsx
@@ -13,38 +13,35 @@ const Portfolio = () => {
     const { userData } = useUser();
     console.log(userData)
     return (
-        
+
         <Layout>
-           <main className="main-portfolio">
-           <section className="s-header">
-            <div>
-                <h1>Sacha Quinteros</h1>
-                <h2>Fullstack Developer</h2>
+            <main className="main-portfolio">
+                <section className="s-header">
+                    <div>
+                        <h1>Sacha Quinteros</h1>
+                        <h2>Fullstack Developer</h2>
 
-                <i className='bx bx-world'></i>
-                <span>Ciudad Autonoma de Buenos Aires, Argentina</span>
-                <ul id="social-list"></ul>
-            </div>
-            <div className="cont-img">
-                {/* <img src="/assets/image/images.png" alt="Imagen de perfil" title="Foto de perfil"> */}
-            </div>
-            
-        </section>
-        <SocialLinks />
-        <section className="s-bio">
-            <h2 className="text-bold">Sobre mi</h2>
-            <p> Cursando diplomatura Fullstack Developer UTN.
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat id repudiandae eaque delectus
-                aliquam eius eligendi quidem cupiditate. Minima.
-            </p>
-        </section>
+                        <i className='bx bx-world'></i>
+                        <span>Ciudad Autonoma de Buenos Aires, Argentina</span>
+                        <ul id="social-list"></ul>
+                    </div>
+                    <div className="cont-img">
+                        {/* <img src="/assets/image/images.png" alt="Imagen de perfil" title="Foto de perfil"> */}
+                    </div>
 
-             <Works />
-             <Education />
-             <Projects />
-           </main>
+                </section>
+                <SocialLinks />
+                <section className="s-bio">
+                    <h2 className="text-bold">Sobre mi</h2>
+                    <p>Actualmente trabajando en Financie Rápido, donde comencé como cadete y ahora me dedico a la gestión de créditos. Mi interés por el desarrollo de software me llevó a estudiar una diplomatura en Full Stack Development en la Universidad Tecnológica Nacional (UTN). Además, he ampliado mis conocimientos a través de cursos en línea en plataformas como Ademass y freeCodeCamp, donde he obtenido certificados en HTML, CSS, JavaScript, GitHub, React y Node.js. </p>
+                </section>
+
+                <Works />
+                <Education />
+                <Projects />
+            </main>
         </Layout>
-        
+
     )
 };
 
